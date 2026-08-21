ba380848129d9794e9b6589d3a710882c5bbeb66
Omar Naman <Smil3yy.naman@gmail.com>
2026-08-21 12:18:07 -0400

    Update project link

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='cli_colors',
       version='0.1',
       description='A light library for coloring terminal output',
-      url='http://github.com/sudo-Smiley/cli_colors',
-      author='sudo-Smiley',
+      url='http://github.com/omarnaman/cli_colors',
+      author='omarnaman',
       license='MIT',
       packages=['cli_colors'],
       zip_safe=False)
